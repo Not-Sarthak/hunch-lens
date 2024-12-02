9aa70c769ee8964a09dc725a7a026c321867632c
Not-Sarthak <notsarthakshah@gmail.com>
2024-12-02 13:29:58 +0530

    feat: ai setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,21 @@
-require("dotenv").config();
+import OpenAI from "openai";
+import { createAssistant } from "./openai/createAssistant.js";
+import { createThread } from "./openai/createThread.js";
+import { createRun } from "./openai/createRun.js";
+import { performRun } from "./openai/performRun.js";
 
-const ANTHROPIC_API_KEY = process.env.ANTHROPIC_API_KEY;
+import "dotenv/config";
+
+async function main() {
+  const client = new OpenAI();
+  const message = "Hello, Hunch. How are you?"
+
+  const assistant = await createAssistant(client);
+  const thread = await createThread(client, message);
+  const run = await createRun(client, thread, assistant.id);
+  const result = await performRun(run, client, thread);
+
+  console.log(result);
+}
+
+main()
--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,17 +1,3 @@
 export const BASE_SEPOLIA_CHAIN_ID = 84532;
-export const mintContractAddress = '0xA3e40bBe8E8579Cd2619Ef9C6fEA362b760dac9f';
-export const mintABI = [
-  {
-    inputs: [
-      {
-        internalType: 'address',
-        name: 'to',
-        type: 'address',
-      },
-    ],
-    name: 'mint',
-    outputs: [],
-    stateMutability: 'public',
-    type: 'function',
-  },
-] as const;
+export const marketFactoryAddress = '0xEa91a34092E5F97b144c36f4fEBf8cA108C4FE84';
+export const marketFactoryAbi = [] as const;
